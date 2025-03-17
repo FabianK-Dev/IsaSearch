3a49e0280553075e6ee2dd05224f993c0a088254
Fabian Kadlez <17363426+Ishidres@users.noreply.github.com>
2025-03-17 17:14:30 +0100

    Read all folders in output_folder/thys/

--- data_preparation.py
+++ data_preparation.py
@@ -4,3 +4,8 @@ git_url = "https://foss.heptapod.net/isa-afp/afp-devel"
 output_folder = "afp-devel-branch-default"
 
 # repo.clone_from(git_url, repo_dir) # TODO
+
+# Use output_folder/thys/ROOTS to get a list of all entries in the AFP
+with open(output_folder + "/thys/ROOTS") as file:
+    lines = [line.rstrip() for line in file]
+    print(lines)
