aa9f2c3d462d33e93a66e36b1a2907c377bac3b6
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-04-14 14:03:20 +0200

    Add extract_theorems_regex(); Better get_entry_files() parsing

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 AFP_FOLDER = "afp-2025-04-13"
 AFP_ROOTS = AFP_FOLDER + "/thys/ROOTS"
@@ -11,26 +12,43 @@ if os.path.exists(AFP_ROOTS):
         for line in entries_file:
             entries.append(line.rstrip())
 
-def load_theorem(theorem_path):
-    if os.path.exists(theorem_path):
-        with open(theorem_path, 'r') as theorem_file:
-            theorem_content = theorem_file.read()
+def extract_theorems_regex(thy_path):
+    if os.path.exists(thy_path):
+        with open(thy_path, 'r') as thy_file:
+            file_content = thy_file.read()
 
-    # TODO
+    # A simple RegEx for testing purposes that extracts and theorem code block from a .thy-file
+    theorem_pattern = r'theorem\s+\w+(\s+:|:)\n(.+\n)*\n\n'
+    theorems = []
+
+    for match in re.finditer(theorem_pattern, file_content):
+        match = match.group()
+        match = match.strip()
+        theorems.append(match)
+
+    return theorems
 
 def get_entry_files(entry):
     entry_folder = AFP_FOLDER + "/thys/" + entry
-    found_files = []
+    found_thy_files = []
+    found_tex_files = []
 
     if os.path.exists(entry_folder):
         for root, _, files in os.walk(entry_folder):
             for file in files:
-                if file.endswith(".thy") or file.endswith(".tex"):
-                    found_files.append({
+                if file.endswith(".thy"):
+                    found_thy_files.append({
                         "root": root,
-                        "file": file
+                        "file": file,
+                        "theorems": extract_theorems_regex(root + "/" + file)
+                    })
+                elif file.endswith(".tex"):
+                    found_tex_files.append({
+                        "root": root,
+                        "file": file,
                     })
 
-    return found_files
+    return { "thy_files": found_thy_files, "tex_files": found_tex_files }
 
-print( get_entry_files("CYK") )
+cyk = get_entry_files("CYK")
+print(cyk)
