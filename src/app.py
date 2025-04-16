7f3091f6ee0d9117c788d1497cd325170dadbb09
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-04-16 16:07:04 +0200

    Update RegEx to be more inclusive

--- src/app.py
+++ src/app.py
@@ -23,8 +23,8 @@ def extract_theorems_regex(thy_path):
         with open(thy_path, 'r') as thy_file:
             file_content = thy_file.read()
 
-    # A simple RegEx for testing purposes that extracts and theorem code block from a .thy-file
-    theorem_pattern = r'(theorem|lemma|corollary)\s+\w+(\s+:|:)\n(.+\n)*\n\n'
+    # A simple RegEx for testing purposes that extracts all theorem code block from a .thy-file
+    theorem_pattern = r'(private)?\s+(theorem|lemma|corollary)(.+\n)*\n\n'
     theorems = []
 
     for match in re.finditer(theorem_pattern, file_content):
