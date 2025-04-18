bc84dfa5288192ca17af7b9f9a6e8cdbfc175186
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-04-18 22:15:49 +0200

    Scraped HTML to CSV extraction (unfinished)

--- benchmark/scrape_theorem_list.py
+++ benchmark/scrape_theorem_list.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import requests
 import csv
+import re
 
 URL = "https://cgi.cse.unsw.edu.au/~kleing/top100/"
 CACHE_FILE = "top100.html"
@@ -21,5 +22,36 @@ with open(CSV_FILE, mode="w", newline="", encoding="utf-8") as file:
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
 
+    i = 0
     for h2 in soup.find_all("h2"):
-        print(h2)
+        id_ = h2.get("id", "")
+        title = h2.get_text(strip=True)
+        title = re.compile(r"^\d+\.\s*").sub("", title)
+        title = re.compile(r"\n+").sub(" ", title)
+
+        code_div = h2.find_next("div", class_="highlight")
+        inner_text = code_div.get_text(strip=False) if code_div else "?"
+        #inner_text = inner_text.split("\n")[0]
+
+        prev_a = h2.find_next("a", class_="uri")
+        href = prev_a.get("href", "") if prev_a else "?"
+
+        if "isa-afp.org" not in href:
+            continue
+
+        i += 1
+        if i > 10:
+            break
+
+        writer.writerow({
+            'ID': id_,
+            'Title': title,
+            'Theorem': inner_text,
+            'Link': href
+        })
+
+        #print("ID:", id_)
+        #print("Titel:", title)
+        print("Theorem:", inner_text)
+        #print("Link:", href)
+        print("-" * 40)
