bf18c46fa131cefc61d636f89b12eceb94c301ac
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-04-24 17:00:00 +0200

    Run find_facts_index on AFP folder

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 config="config.json"
 isabelle_binary_file=$(jq -r '.isabelle_binary_file' "$config")
+afp_folder=$(jq -r '.afp_folder' "$config")
 
 echo "Install Isabelle and the AFP"
 python3 -m src.installation
@@ -10,6 +11,6 @@ echo "Install Isabelle components"
 echo "Isabelle binary is located at $isabelle_binary_file"
 
 $isabelle_binary_file components -a
-$isabelle_binary_file find_facts_index
+$isabelle_binary_file find_facts_index -v -A "$afp_folder/" $(cat "$afp_folder/thys/ROOTS")
 
 python3 -m src.app
