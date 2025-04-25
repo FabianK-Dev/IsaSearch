cc79d0cef79d7db60348cb6a8017a569d0ff484a
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-04-25 15:55:43 +0200

    Delete methods that already exist in documents.py; add imports

--- src/app.py
+++ src/app.py
@@ -13,6 +13,9 @@ import torch
 import time
 import pysolr
 
+from solr import connect_solr
+from documents import extract_docs, fetch_all_docs
+
 print("Downloading stopwords corpus and check for updates...")
 nltk.download('stopwords')
 
@@ -28,39 +31,6 @@ with open("config.json", "r") as file:
 AFP_FOLDER = config["afp_folder"]
 CACHE_FOLDER = config["cache_folder"]
 ENTRY_DB_CACHE = f"{CACHE_FOLDER}/entry_db_cache.json"
-
-def extract_docs(doc):
-    doc_str = doc["src"]
-
-    # Replace newlines through spaces
-    doc_str = re.compile(r"\n").sub(" ", doc_str)
-
-    # Replace two or more white spaces through single whitespace
-    doc_str = re.compile(r"\s+").sub(" ", doc_str).strip()
-    
-    doc_str = doc["theory"] + " " + doc_str # Prepend the theory name
-    return doc["id"], doc_str
-
-def fetch_all_docs(solr):
-    document_ids = []
-    documents = []
-
-    docs_per_page = 1000
-    results = solr.search("command:theorem OR command:lemma OR command:corollary", start=0, rows=docs_per_page)
-    max_docs = results.raw_response['response']['numFound']
-
-    for result in results:
-        extract_docs(result)
-    
-    pages = math.ceil(max_docs / docs_per_page)
-    for i in range(pages):
-        print(f"Fetching page {i+1} of {pages} pages...")
-        results = solr.search("command:theorem OR command:lemma OR command:corollary", start=0, rows=docs_per_page)
-
-        for result in results:
-            doc_id, doc_str = extract_docs(result)
-            document_ids.append(doc_id)
-            documents.append(doc_str)
 
 if os.path.isfile(ENTRY_DB_CACHE):
     print("Cached entry_db_cache.json already exists. Loading...")
