c187812cedceb5167a00b33ca4d75e6c3d243b0c
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-04-25 18:22:09 +0200

    Add docs_by_ids() method

--- src/solr.py
+++ src/solr.py
@@ -8,3 +8,15 @@ def connect_solr(config):
     solr.ping() # Health check
 
     return solr
+
+def docs_by_ids(solr, ids):
+    id_strings = []
+
+    for _id in ids:
+        id_string = "id:" + _id
+        id_strings.append(id_string)
+
+    id_query = " OR ".join(id_strings)
+    results = solr.search(id_query, start=0, rows=100)
+
+    return results
