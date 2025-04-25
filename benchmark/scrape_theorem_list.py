22bb1210e3bc4148f3ae56a75f565def39dc24be
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-04-26 01:34:01 +0200

    Save statistics about used entries
    This allows me to create a find_facts_index with only required entries

--- benchmark/scrape_theorem_list.py
+++ benchmark/scrape_theorem_list.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import requests
 import csv
+import json
 import re
 
 URL = "https://cgi.cse.unsw.edu.au/~kleing/top100/"
@@ -17,12 +18,17 @@ if not os.path.exists(CACHE_FILE):
 with open(CACHE_FILE, "r") as file:
     soup = BeautifulSoup(file, "html.parser")
 
+scrape_statistics = {
+    "unique_entries": [],
+    "unknown_entries": 0,
+    "unknwon_entry_urls": []
+}
+
 with open(CSV_FILE, mode="w", newline="", encoding="utf-8") as file:
-    fieldnames = ['ID', 'Title', 'Theorem', 'Link', 'Title query']
+    fieldnames = ['ID', 'Title', 'Theorem', 'Link', 'Entry', 'Title query']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
 
-    i = 0
     for h2 in soup.find_all("h2"):
         id_ = h2.get("id", "")
         title = h2.get_text(strip=True)
@@ -35,19 +41,25 @@ with open(CSV_FILE, mode="w", newline="", encoding="utf-8") as file:
 
         prev_a = h2.find_next("a", class_="uri")
         href = prev_a.get("href", "") if prev_a else "?"
+        
+        if "/entries/" in href:
+            entry = href.split("/entries/")[1].split(".")[0]
+            
+            if entry not in scrape_statistics["unique_entries"]:
+                scrape_statistics["unique_entries"] = scrape_statistics["unique_entries"] + [entry]
+        else:
+            entry = "?"
+            scrape_statistics["unknown_entries"] += 1
 
-        if "isa-afp.org" not in href:
-            continue
-
-        i += 1
-        if i > 10:
-            break
+            if href not in scrape_statistics["unknwon_entry_urls"]:
+                scrape_statistics["unknwon_entry_urls"] = scrape_statistics["unknwon_entry_urls"] + [href]
 
         writer.writerow({
             'ID': id_,
             'Title': title,
             'Theorem': inner_text,
             'Link': href,
+            'Entry': entry,
             'Title query': title
         })
 
@@ -56,3 +68,6 @@ with open(CSV_FILE, mode="w", newline="", encoding="utf-8") as file:
         print("Theorem:", inner_text)
         print("Link:", href)
         print("-" * 40)
+
+with open("scrape_statistics.json", "w") as outfile:
+    json.dump(scrape_statistics, outfile, indent=4)
