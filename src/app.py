716464f07f1b89b250d80afb3672a9261291c9db
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-04-25 15:38:36 +0200

    Working on extracting documents

--- src/app.py
+++ src/app.py
@@ -35,12 +35,30 @@ solr = pysolr.Solr(config["solr_core_url"], always_commit=True, timeout=10)
 print("Ping Solr for health check...")
 solr.ping() # Health check
 
+def extract_docs(doc):
+    doc_str = doc["session"] + doc["src"] # TODO
+    return doc["id"], doc_str
+
 def fetch_all_docs(solr):
+    document_ids = []
+    documents = []
+
     docs_per_page = 1000
     results = solr.search("command:theorem OR command:lemma OR command:corollary", start=0, rows=docs_per_page)
     max_docs = results.raw_response['response']['numFound']
+
+    for result in results:
+        extract_docs(result)
     
     pages = math.ceil(max_docs / docs_per_page)
+    for i in range(pages):
+        print(f"Fetching page {i+1} of {pages} pages...")
+        results = solr.search("command:theorem OR command:lemma OR command:corollary", start=0, rows=docs_per_page)
+
+        for result in results:
+            doc_id, doc_str = extract_docs(result)
+            document_ids.append(doc_id)
+            documents.append(doc_str)
 
 if os.path.isfile(ENTRY_DB_CACHE):
     print("Cached entry_db_cache.json already exists. Loading...")
