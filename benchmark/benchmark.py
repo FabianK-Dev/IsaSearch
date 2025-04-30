5e276f3698991a17a65c644892636712927b9cb5
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-04-30 18:55:20 +0200

    Make cross_encoder optional

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,6 +1,6 @@
 from src.solr import connect_solr, docs_by_ids
 from src.documents import build_document_tree
-from src.embeddings import encode_embeddings, load_models, search
+from src.embeddings import encode_embeddings, load_models, search, search_results_to_docs
 from src.nltk_setup import init_nltk_corpora
 
 import json
@@ -13,8 +13,11 @@ with open("config.json", "r") as file:
 init_nltk_corpora()
 solr = connect_solr(config)
 document_tree = build_document_tree(config, solr)
-bi_encoder, cross_encoder = load_models('sentence-transformers/all-MiniLM-L6-v2', 'cross-encoder/ms-marco-MiniLM-L6-v2')
+bi_encoder, cross_encoder = load_models(bi_encoder_model='flax-sentence-embeddings/st-codesearch-distilroberta-base', cross_encoder_model='nomic-ai/CodeRankEmbed')
 encoded_embeddings = encode_embeddings(config, document_tree, bi_encoder)
 
-results = search("Any consistent formal system F within which a certain amount of elementary arithmetic can be carried out is incomplete", encoded_embeddings, bi_encoder, cross_encoder, document_tree)
-#results = search("Square Root of 2 is Irrational", encoded_embeddings, bi_encoder, cross_encoder, document_tree)
+results = search("Any consistent formal system F within which a certain amount of elementary arithmetic can be carried out is incomplete", encoded_embeddings, document_tree, bi_encoder, cross_encoder=None)
+docs = search_results_to_docs(results, solr)["results"]
+
+for doc in docs[:20]:
+    print(str(float(doc["score"])) + ": " + doc["id"])
