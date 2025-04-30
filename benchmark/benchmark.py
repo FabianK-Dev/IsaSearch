70337fae29325b6f5ec1679d5f74f4e757f442a3
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-04-30 13:57:48 +0200

    Apply changes from app.py

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -13,8 +13,8 @@ with open("config.json", "r") as file:
 init_nltk_corpora()
 solr = connect_solr(config)
 document_tree = build_document_tree(config, solr)
-encoded_embeddings = encode_embeddings(config, document_tree)
-bi_encoder, cross_encoder = load_models()
+bi_encoder, cross_encoder = load_models('sentence-transformers/all-MiniLM-L6-v2', 'cross-encoder/ms-marco-MiniLM-L6-v2')
+encoded_embeddings = encode_embeddings(config, document_tree, bi_encoder)
 
 results = search("Any consistent formal system F within which a certain amount of elementary arithmetic can be carried out is incomplete", encoded_embeddings, bi_encoder, cross_encoder, document_tree)
 #results = search("Square Root of 2 is Irrational", encoded_embeddings, bi_encoder, cross_encoder, document_tree)
