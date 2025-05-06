841a1ce1186c50310a18c77e98585dcc65850355
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-05-06 16:02:53 +0200

    Edit path

--- solr_db.sh
+++ solr_db.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8983:8983 -v /home/fabian/.isabelle/Isabelle2025/find_facts/solr/local:/opt/solr/server/solr/local solr:latest solr-precreate local /opt/solr/server/solr/local
+docker run -d -p 8983:8983 -v /home/fabian/.isabelle/Isabelle2025/find_facts_prod/solr/local:/opt/solr/server/solr/local solr:latest solr-precreate local /opt/solr/server/solr/local
