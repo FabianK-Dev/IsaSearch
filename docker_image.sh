1bef5ee53ba39a7180fa18535a6165d76ddb08fa
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-06-03 15:49:07 +0200

    Add LLM name as build argument

--- docker_image.sh
+++ docker_image.sh
@@ -2,5 +2,5 @@ cp .gitignore .dockerignore
 echo "" >> .dockerignore
 echo "Dockerfile" >> .dockerignore
 
-docker build -t afp-ai-search . \
+docker build --build-arg llm_name=microsoft/Phi-3.5-mini-instruct -t afp-ai-search . \
     && docker run --gpus all -it afp-ai-search
