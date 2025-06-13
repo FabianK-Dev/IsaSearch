acb6bffac34147cd0d94187c2a0173489a342f56
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-06-13 17:50:41 +0200

    Rename variable

--- benchmark/metrics.py
+++ benchmark/metrics.py
@@ -19,14 +19,14 @@ def is_correct_target(result_doc, target_identifier):
 
 # For a given query, is the target_identifier among the top-k search results? => 1 (yes) / 0 (false)
 top_k = 10
-def top_k_accuracy(results, target_id):
+def top_k_accuracy(results, target_identifier):
     for i, result in enumerate(results):
         if i >= top_k: # i starts at 0 => if i == top_k we can gurantee that the result was not within the first k results
             return 0
 
-        if is_correct_target(result, target_id):
+        if is_correct_target(result, target_identifier):
             return 1
-    
+
     return 0
 
 # Calculates a relevance scale of the retrieved documents
