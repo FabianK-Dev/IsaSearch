05b821bfe1c17f06dafc7983cfd2dbb9a81fd18d
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-07-03 15:40:08 +0200

    Fix path

--- run_full_benchmark.sh
+++ run_full_benchmark.sh
@@ -10,42 +10,42 @@ then
 fi
 
 # Don't add metadata to embeddings, refine the query but don't append the original user query
-rm .cache/.cache/document_index.json
+rm .cache/document_index.json
 jq '.add_metadata = false' config.json > tmp && mv tmp config.json
 jq '.add_user_query = false' config.json > tmp && mv tmp config.json
 jq '.benchmark_search_refine = true' config.json > tmp && mv tmp config.json
 python -m benchmark.benchmark
 
 # Don't add metadata to embeddings, but only use the original user query without query refinement
-rm .cache/.cache/document_index.json
+rm .cache/document_index.json
 jq '.add_metadata = false' config.json > tmp && mv tmp config.json
 jq '.add_user_query = false' config.json > tmp && mv tmp config.json
 jq '.benchmark_search_refine = false' config.json > tmp && mv tmp config.json
 python -m benchmark.benchmark
 
 # Don't add metadata to embeddings, use query refinement and add the original user query
-rm .cache/.cache/document_index.json
+rm .cache/document_index.json
 jq '.add_metadata = false' config.json > tmp && mv tmp config.json
 jq '.add_user_query = true' config.json > tmp && mv tmp config.json
 jq '.benchmark_search_refine = true' config.json > tmp && mv tmp config.json
 python -m benchmark.benchmark
 
 # Add metadata to embeddings, refine the query but don't append the original user query
-rm .cache/.cache/document_index.json
+rm .cache/document_index.json
 jq '.add_metadata = true' config.json > tmp && mv tmp config.json
 jq '.add_user_query = false' config.json > tmp && mv tmp config.json
 jq '.benchmark_search_refine = true' config.json > tmp && mv tmp config.json
 python -m benchmark.benchmark
 
 # Add metadata to embeddings, but only use the original user query without query refinement
-rm .cache/.cache/document_index.json
+rm .cache/document_index.json
 jq '.add_metadata = true' config.json > tmp && mv tmp config.json
 jq '.add_user_query = false' config.json > tmp && mv tmp config.json
 jq '.benchmark_search_refine = false' config.json > tmp && mv tmp config.json
 python -m benchmark.benchmark
 
 # Add metadata to embeddings, use query refinement and add the original user query
-rm .cache/.cache/document_index.json
+rm .cache/document_index.json
 jq '.add_metadata = true' config.json > tmp && mv tmp config.json
 jq '.add_user_query = true' config.json > tmp && mv tmp config.json
 jq '.benchmark_search_refine = true' config.json > tmp && mv tmp config.json
