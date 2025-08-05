15f4790332558f40f0b057dc55c2ac0a6f7eddd9
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2025-08-05 16:47:29 +0200

    Update

--- docker_build.sh
+++ docker_build.sh
@@ -8,8 +8,11 @@ sed -i '/assets_extracted/d' .dockerignore
 echo "Extracting assets to assets_extracted/"
 mkdir -p assets_extracted
 
-tar -xf ./assets/afp-2025-branch-default.tar.gz -C assets_extracted/
-tar -xf ./assets/chroma_storages.tar.gz -C assets_extracted/
-tar -xf ./assets/find_facts.tar.gz -C assets_extracted/
+# --keep-newer-files makes sure tar.gz files are only extracted if they are newer then folders in assets_extracted/
+tar --keep-newer-files -xf ./assets/afp-2025-branch-default.tar.gz -C assets_extracted/
+tar --keep-newer-files -xf ./assets/chroma_storages.tar.gz -C assets_extracted/
+tar --keep-newer-files -xf ./assets/find_facts.tar.gz -C assets_extracted/
 
 docker build -t gitlab.lrz.de:5005/kadlez/afp-ai-search .
+docker images gitlab.lrz.de:5005/kadlez/afp-ai-search
+docker history gitlab.lrz.de:5005/kadlez/afp-ai-search
