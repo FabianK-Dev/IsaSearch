6be481c9d49ef499901627a0f08acac143c4277f
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2026-01-25 14:35:27 +0100

    Also add sentence-transformers to install package

--- install_packages.sh
+++ install_packages.sh
@@ -1,2 +1,2 @@
-pip install vllm transformers pandas nltk beautifulsoup4 requests flask waitress chromadb gpt4all gpt4all[cuda] pysolr tqdm torch pre-commit
+pip install vllm transformers pandas nltk beautifulsoup4 requests flask waitress chromadb gpt4all gpt4all[cuda] pysolr tqdm torch pre-commit sentence-transformers
 pip freeze > requirements.txt
