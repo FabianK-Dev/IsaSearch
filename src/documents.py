72890a377a05f82ae3cb3e699c9f11d6a185b4af
Fabian Kadlez <F.Kadlez@campus.lmu.de>
2026-01-25 15:40:06 +0100

    Load list of commands for search query from config.json

--- src/documents.py
+++ src/documents.py
@@ -69,7 +69,7 @@ def fetch_all_docs(solr, config):
 
     docs_per_page = 10000
     results = solr.search(
-        "command:theorem OR command:lemma OR command:corollary OR command:proposition",
+        config["solr_query"],
         start=0,
         rows=docs_per_page,
     )
@@ -85,7 +85,7 @@ def fetch_all_docs(solr, config):
     for i in range(1, pages):
         print(f"Fetching page {i + 1} of {pages} pages...")
         results = solr.search(
-            "command:theorem OR command:lemma OR command:corollary OR command:proposition",
+            config["solr_query"],
             start=i * docs_per_page,
             rows=docs_per_page,
         )
